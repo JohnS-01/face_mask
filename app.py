e37dbe3372f5600b94e0fdfc0a5f038fdf4701ff
Glynnis Thomas <glynnisthomas@gmail.com>
2018-01-21 15:03:49 -0600

    Added flask, flask-sqlalchemy, and pymysql -- set up db. created file for models.

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://face-mask:interface44#@localhost:8889/face-mask'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+
+# @app.route('/')
+# def index():
+#     return 200
+
+app.run()
