deefa7c4ec06f7f682d069949943da2580ff6654
Glynnis Thomas <glynnisthomas@gmail.com>
2018-01-21 18:03:43 -0600

    corrected variable names

--- server/hashutils.py
+++ server/hashutils.py
@@ -8,18 +8,19 @@ def make_salt():
     """ Creates the salt that is tacked onto the end of the password to improve encryption. """
     return ''.join([random.choice(string.ascii_letters) for f in range(5)])
 
-def make_pw_hash(pw, salt=None):
+def make_pw_hash(password, salt=None):
     """ Creates the actual password hash with the salt included.
-        Takes the entered password as a string, and an optional salt, also a string, which defaults to None. """
+        Takes the entered password as a string.
+        Takes an optional salt, also a string, which defaults to None. """
     if not salt:
         salt = make_salt()
-    new_hash = hashlib.sha256(str.encode(pw + salt)).hexdigest()  # creates a unique user hash from the combined pw and salt
+    new_hash = hashlib.sha256(str.encode(password + salt)).hexdigest()  # creates a unique user hash from the combined pw and salt
     return '{0},{1}'.format(new_hash, salt)  # returns the new hash, including the salt so user can be verified later
 
-def check_pw_hash(pw, hash):
+def check_pw_hash(password, hashy):
     """ Checks the entered password against the hash stored for the user in the DB.
         Takes the entered password as a string, and the original hash stored for the user. """
-    salt = hash.split(',')[1]  # gets the users original salt so that when it creates the new hash it will match
-    if make_pw_hash(pw, salt) == hash:
+    salt = hashy.split(',')[1]  # gets the users original salt so that when it creates the new hash it will match
+    if make_pw_hash(password, salt) == hash:
         return True
     return False
