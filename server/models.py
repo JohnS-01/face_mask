06a0f21990c960f725ad216f1477d8ed07cbc911
Glynnis Thomas <glynnisthomas@gmail.com>
2018-01-21 22:19:09 -0600

    Attempted to debug weird error

--- server/models.py
+++ server/models.py
@@ -1,15 +1,18 @@
 """ Model Classes """
 
 from app import db
+from hashutils import make_pw_hash
 
 class User(db.Model):
+    """ This will be the User class and will hold information regarding user accounts. """
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)  # will not allow a user to register twice with the same email address
     pwd_hash = db.Column(db.String(300))
 
-    def __init__(self, email, pw):
+    def __init__(self, email, password):
         self.email = email
-        self.pwd_hash = 0 # will set up hashutils next and return to this function
+        self.pwd_hash = make_pw_hash(password)
 
     def __repr__(self):
         return f'(self.email)'
+    
