6fdbcd7cf4802fe80d59faa98bf0f672f10f587e
Glynnis Thomas <glynnisthomas@gmail.com>
2018-02-07 20:57:36 -0600

    Changed out returns with json objects, partway through testing

--- server/main.py
+++ server/main.py
@@ -1,38 +1,79 @@
 """ Main Controller """
 
 import re
-from flask import request, redirect, session, flash, render_template
+from flask import request, redirect, session, flash, render_template, Response, jsonify
 from app import app, db
 from models import User
 from hashutils import make_pw_hash, check_pw_hash
 
+"""
+@app.route('/test', methods=['GET'])
+def testing():
+    sample_data = {
+        'hello' : 'world',
+    }
+    resp = jsonify(sample_data)
+    resp.status_code = 200
+    return resp
+
 @app.before_request
 def require_login():
-    """ Lists pages that require user to be logged in to view. """
-    allowed_routes = ['login', 'signup']
+   #Lists pages that require user to be logged in to view.
+    allowed_routes = ['login', 'signup', 'test']
     if request.endpoint not in allowed_routes and 'email' not in session:
         return redirect('/login')
+"""
 
-@app.route('/login', methods=['GET','POST'])
+# TODO : Add error handlers
+
+@app.route('/login', methods=['GET'])
 def login():
-    """ Checks user login information against the DB and logs them in if it's correct. """
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(email=email).first()
-        if user and check_pw_hash(password, user.pwd_hash):
-            session['email'] = email
-            return redirect('/home')
-        elif user and not check_pw_hash(password, user.pwd_hash):
-            flash("Password is incorrect!", 'error')
-        else: 
-            flash("Email does not exist, please sign up!", 'error')
-    
-    return render_template('/login.html')
+   # Checks user login information against the DB and logs them in if it's correct. 
+    data = {
+        'status' : 403, 
+        'message' : 'User not logged in.'
+    }
+    resp = jsonify(data)
+    resp.status_code = 403
+    return resp
 
-@app.route('/signup', methods=['GET','POST'])
+@app.route('/login', methods=['POST'])
+def new_user():
+    email = request.form['email']
+    password = request.form['password']
+    user = User.query.filter_by(email=email).first()
+    vs_pw = user.pwd_hash
+    if user and check_pw_hash(password, vs_pw):
+        data = {
+            'email' : email,
+            'status' : 200,
+            'verified' : True
+        }
+        resp = jsonify(data)
+        resp.status_code = 200
+        return resp
+
+    elif user and not check_pw_hash(password, user.pwd_hash):
+        data = {
+            'status' : 401,
+            'message' : 'Password is incorrect.'
+        }
+        resp = jsonify(data)
+        resp.status_code = 401
+        return resp
+
+    else:
+        data = {
+            'status' : 401,
+            'message' : 'Email does not exist, please sign up.'
+        }
+        resp = jsonify(data)
+        resp.status_code = 401
+        return resp
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    """ Allows the user to create an accout -- we can add to this later. """
+    #Allows the user to create an accout -- we can add to this later.
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
@@ -42,29 +83,78 @@ def signup():
         email_check = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")  # sourced from http://emailregex.com
         result = email_check.match(email)
         if not result:
-            flash("That is not a valid email.", 'error')
-        elif len(password) < 3 or len(password) < 20 or " " in password:
-            flash("That is not a valid password.", 'error')
-        elif password != verify:
-            flash("Passwords do not match.", 'error')
-        else:  # Check if user already exists
-            existing_user = User.query.filter_by(email=email).first()
+            data = {
+                'status' : 401,
+                'message' : 'That is not a valid email.'
+            }
+            resp = jsonify(data)
+            resp.status_code = 401
+            return resp
 
-            if existing_user:
-                flash("That email is already in use.", 'error')
+        elif len(password) < 3 or len(password) > 20 or " " in password:
+            data = {
+                'status' : 401,
+                'message' : 'That is not a valid password.'
+            }
+            resp = jsonify(data)
+            resp.status_code = 401
+            return resp
+
+        elif password != verify:
+            data = {
+                'status' : 401,
+                'message' : 'Passwords do not match.'
+            }
+            resp = jsonify(data)
+            resp.status_code = 401
+            return resp
+        
+        else:  # Check if user already exists
+            if User.query.filter_by(email=email).first() is not None:
+                data = {
+                    'status' : 401,
+                    'message' : 'That email is already in use.'
+                }
+                resp = jsonify(data)
+                resp.status_code = 401
+                return resp
+                
             else:  # Create new user
                 new_user = User(email, make_pw_hash(password))
                 db.session.add(new_user)
                 db.session.commit()
                 session['email'] = email
-                return redirect('/home')  
-    return render_template("/signup.html")              
+
+                data = {
+                    'email' : new_user.email, 
+                    'status' : 200,
+                    'id' : new_user.id,
+                    'verified' : True
+                    # 'location' : url for user if we wanna add this later
+                }
+                resp = jsonify(data)
+                resp.status_code = 200
+                return resp
+    data = {
+        'status' : 403, 
+        'message' : 'User not logged in.'
+    }
+    resp = jsonify(data)
+    resp.status_code = 403
+    return resp
 
 @app.route('/logout', methods=['GET'])
 def logout():
-    """ Logs the user out / ends the session. """
+   # Logs the user out / ends the session.
     del session['email']
-    return redirect('/login')
+
+    data = {
+        'status' : 403, 
+        'message' : 'User not logged in.'
+    }
+    resp = jsonify(data)
+    resp.status_code = 403
+    return resp
 
 @app.route('/home')
 def index1():
