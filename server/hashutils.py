2e0244da2c4b2cbe1f36a17a49c131840dc7fd4b
Glynnis Thomas <glynnisthomas@gmail.com>
2018-02-11 15:49:04 -0600

    In process of debugging password hashing

--- server/hashutils.py
+++ server/hashutils.py
@@ -9,7 +9,7 @@ def make_salt():
     """ Creates the salt that is tacked onto the end of the password to improve encryption. """
     return ''.join([random.choice(string.ascii_letters) for f in range(5)])
 
-def make_pw_hash(password, salt=None):
+def make_pw_hash(password, salt='eIoIk'):
     """ Creates the actual password hash with the salt included.
         Takes the entered password as a string.
         Takes an optional salt, also a string, which defaults to None. """
@@ -22,7 +22,11 @@ def check_pw_hash(password, hashy):
     """ Checks the entered password against the hash stored for the user in the DB.
         Takes the entered password as a string, and the original hash stored for the user. """
     salt = hashy.split(',')[1]  # gets the users original salt so that when it creates the new hash it will match
-    if make_pw_hash(password, salt) == hash:
+    print(password)
+    print(salt)
+    print(hashy)
+    print( make_pw_hash(password, salt))
+    if make_pw_hash(password, salt) == hashy:
         return True
     return False
 
