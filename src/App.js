2f77e31ee79c8f877c7da3ef0e4be7dd2506efe5
JohnS-01 <John_Schmitz01@outlook.com>
2018-03-01 20:20:46 -0600

    Added Join and Login pages for routing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,85 @@ import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 import { Button, Container, Header } from 'semantic-ui-react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
+import {Join} from './components/Pages/Join';
+
+const Home = () => (
+  <div></div>
+)
+
+const SignUp = () => (
+  <div>
+    <h2>Sign Up</h2>
+  </div>
+)
+
+const LogIn= () => (
+  <div>
+    <h2>Log In</h2>
+  </div>
+)
+
+/*
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React TEST
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:topicId`} component={Topic}/>
+    <Route exact path={match.path} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+*/
 class App extends Component {
   render() {
     return (
   <div classNameName="App">
+      {/*Routing to connect hyper links to other pages */ }
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/login">Log In</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" component={Home}/>
+          <Route path="/signup" component={Join}/>
+          <Route path="/login" component={LogIn}/>
+        </div>
+      </Router>
       <div className="ui form">
           {/* Question 1 */}
           <div className="inline fields">
