89f59805e824af8ce53a3ca79b3febe28e645449
JohnS-01 <John_Schmitz01@outlook.com>
2018-06-03 17:53:50 -0500

    Removed uneeded css imports from type of the page

--- src/components/pages/SurveyForm.js
+++ src/components/pages/SurveyForm.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import 'semantic-ui-css/semantic.min.css';
-
 import { surveyData } from '../../userApi';
-//import SignUpForm from '../components/pages/SurveyForm';
 
 class Survey extends Component {
 
